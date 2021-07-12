f74e05189a570b084765209371a8e28f521df9ce
RanjithDevaraj54 <ranjith.ei19@bitsathy.ac.in>
2021-07-12 15:35:17 +0530

    included navigation php file for navigation bar

---
+++ navbar.php
@@ -0,0 +1,28 @@
+
+<style>
+        .navbar-nav {
+            margin-left: 650px;
+         
+        }
+        .navbar{
+          margin-right:1px;
+          
+        }
+    </style>
+<nav class="navbar navbar-expand navbar-light bg-light  ml-auto">
+<a class="navbar-brand  nav-link" href="index.php" style="color: #A3A3A3; font-size:30px; font-family:Poppins;">BHARAT BANK</a><br></br>
+<a class="navbar-brand navbar nav-link"  style="color: #A3A3A3; font-size:20px;"> </a>
+      
+<div class="collapse navbar-collapse navbar navbar-expand navbar-light bg-light  ml-auto" id="collapsenav">
+            <ul class="navbar-nav ">
+            
+              <li class="nav-item">
+                <a class="nav-link" href="viewusers.php" style="color: black; font-size:20px; text-align:right;font-family:Poppins; position:absolute; top: -10px; right:50px;">USERS </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="transactionDetails.php" style="color: black; font-size:20px; text-align:right;font-family:Poppins; position:absolute; top: -10px; right:200px;">HISTORY</a>
+              </li>
+      </div>
+       </body>
+       </nav>
+       </html>
