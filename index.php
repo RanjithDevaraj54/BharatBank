26833a3a6d1f38e1271a8e9e7c1c9f804feeff40
RanjithDevaraj54 <ranjith.ei19@bitsathy.ac.in>
2021-07-12 12:39:47 +0530

    index file added

---
+++ index.php
@@ -0,0 +1,47 @@
+<!Doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="preconnect" href="https://fonts.gstatic.com" />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&display=swap"
+      rel="stylesheet"
+    />
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <title>Banking System</title>
+  </head>
+
+  <body style="background-color:white;">
+  <header>
+  <?php
+  include 'navbar.php';
+  ?></header>
+<main>
+  <style>
+  body{
+    background-image: url('SPL2.jpg');
+    background-repeat: no-repeat;
+    background-attachment: fixed;
+    background-position: center;
+    background-repeat: no-repeat;
+  
+  }
+  
+</style>
+<div class="container"style="font-family:Poppins; font-size:25px; position:absolute; top: 180px; left:320px;">
+<br>
+<p >
+
+Investment-Banking-Transactions are Geared for SUCCESS.
+
+</p>
+</div>
+<div>
+<center>
+<h5 style = "position:absolute; bottom: 20px; left:600px;font-family:Poppins;">&copy; 2021, All rights reserved | Built by <a href="https://www.linkedin.com/in/ranjith-devaraj/" target="_blank">Ranjith Devaraj</a></h1>	
+<h5 style = "position:absolute; bottom: 0; left:640px;font-family:Poppins;">&Web Dev Intern @ <a href="https://www.linkedin.com/company/the-sparks-foundation/" target="_blank">The Sparks Foundation</a></h1>
+</center>
+</main>
+  </body>
+</html>
